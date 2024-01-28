93d063f10400a045b9f9262429915f5e896df1fc
Gustavo da Costa <gustavocosta.nh@gmail.com>
2024-01-28 00:14:19 -0300

    First part working

--- app.js
+++ app.js
@@ -1,74 +1,81 @@
-let listaDeNumerosSorteados = [];
-let tentativas = 0;
-let numeroMaximo = 1000;
-let numeroSecreto = gerarNumeroAleatorio();
-
-
-responsiveVoice.OnVoiceReady = function () {
-	        console.log("As vozes estão prontas!");
-			mensagemInicial();
-	    }
-
-function alterarTag (tag, texto1) {
-	let campo = document.querySelector(tag);
+function alterarId (tag, texto1) {
+	let campo = document.getElementById(tag);
 	campo.innerHTML = texto1;
-	// Responsive voice não se encontra aqui devido a um erro no qual se duas strings fossem transmitidas para ele em sequência ele falaria apenas a segunda.
 }
 
-function gerarNumeroAleatorio() {
-	let numeroSorteado = parseInt(Math.random() * numeroMaximo +1);
+function criptografarTexto(texto) {
+	let textoTemp = [];
+	for (let i=0; i < texto.value.length; i++ ) {
+		if (texto.value[i] == "e"){
+			textoTemp.push("enter");
+		}
+		else if (texto.value[i] == "i"){
+			textoTemp.push("imes");
+		}
+		else if (texto.value[i] == "a"){
+			textoTemp.push("ai");
+		}
+		else if (texto.value[i] == "o"){
+			textoTemp.push("ober");
+		}
+		else if (texto.value[i] == "u"){
+			textoTemp.push("ufat");
+		}
+		else {
+			textoTemp.push(texto.value[i]);
+		}
 
-	if (listaDeNumerosSorteados.length == numeroMaximo){
-		listaDeNumerosSorteados = [];
-		alert("O jogo foi reiniciado!");
 	}
+	// console.log(texto.value.length)
+	return textoTemp.join("");
+}
 
-	if (listaDeNumerosSorteados.includes(numeroSorteado)){
-		return gerarNumeroAleatorio();
+function descriptografarTexto(texto) {
+	let textoTemp = [];
+	for (let i=0; i < texto.value.length; i++ ) {
+		if (texto.value[i] == "e" && texto.value[i+1] == "n" && texto.value[i+2] == "t" && texto.value[i+3] == "e" && texto.value[i+4] == "r"){
+			textoTemp.push("e");
+			i = i + 4;
+		}
+		else if (texto.value[i] == "i" && texto.value[i+1] == "m" && texto.value[i+2] == "e" && texto.value[i+3] == "s"){
+			textoTemp.push("i");
+			i = i + 3;
+		}
+		else if (texto.value[i] == "a" && texto.value[i+1] == "i"){
+			textoTemp.push("a");
+			i = i + 1;
+		}
+		else if (texto.value[i] == "o" && texto.value[i+1] == "b" && texto.value[i+2] == "e" && texto.value[i+3] == "r"){
+			textoTemp.push("o");
+			i = i + 3;
+		}
+		else if (texto.value[i] == "u" && texto.value[i+1] == "f" && texto.value[i+2] == "a" && texto.value[i+3] == "t"){
+			textoTemp.push("u");
+			i = i + 3;
+		}
+		else {
+			textoTemp.push(texto.value[i]);
+		}
+
 	}
-	listaDeNumerosSorteados.push(numeroSorteado);
-	return numeroSorteado;
-
+	// console.log(texto.value.length)
+	return textoTemp.join("");
 }
 
-function verificarChute()  {
-	tentativas++;
-	let chute = parseInt(document.querySelector('input').value);
-	if (chute == numeroSecreto) {
-		let texto = tentativas == 1 ? `Parabéns, você acertou em apenas ${tentativas} tentativa.` : `Parabéns, você acertou em apenas ${tentativas} tentativas.`
-		alterarTag('p', texto);
-		alterarTag('h1', 'Acertou!')
 
-		// Fiz dessa forma e não dentro da função alterarTag devido a um erro no responsiveVoice que falava apenas o segundo texto mandado.
 
-		responsiveVoice.speak('Acertou!' + texto, 'Brazilian Portuguese Female', {rate:1.2});
-		document.getElementById('reiniciar').removeAttribute('disabled');
-	}
-	else {
-		let msg = chute < numeroSecreto ? `O número é maior que ${chute}` : `O número é menor que ${chute}`
-		alterarTag('p', msg);
-		responsiveVoice.speak(msg, 'Brazilian Portuguese Female', {rate:1.2});
-		limparOCampo();
-	}
+function criptografar() {
+	let texto = document.getElementById("principal__container__entrada");
+	// console.log(texto.value)
+	let textoCriptografado = criptografarTexto(texto);
+	alterarId("principal__container__saida", textoCriptografado);
 }
 
-function limparOCampo () {
-	let limpo = document.querySelector('input');
-	limpo.value = '';
+function descriptografar() {
+	let texto = document.getElementById("principal__container__entrada");
+	// console.log(texto.value)
+	let textoDescriptografado = descriptografarTexto(texto);
+	alterarId("principal__container__saida", textoDescriptografado);
 }
 
-function reiniciarJogo() {
-	numeroSecreto = gerarNumeroAleatorio();
-	tentativas = 0;
-	limparOCampo();
-	mensagemInicial();
-	document.getElementById('reiniciar').setAttribute('disabled', true);
-
-}
-
-function mensagemInicial() {
-	alterarTag('h1', 'Jogo do número secreto!');
-	alterarTag('p', `Escolha um número entre 1 e ${numeroMaximo}`);
-	responsiveVoice.speak('Jogo do número secreto!' + `Escolha um número entre 1 e ${numeroMaximo}`, 'Brazilian Portuguese Female', {rate:1.2});
-}
 
