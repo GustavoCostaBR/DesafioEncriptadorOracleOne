313c06c28b34e46bd5d7b3c86778f0d2de97d1c6
Gustavo da Costa <gustavocosta.nh@gmail.com>
2024-01-29 12:24:34 -0300

    Image and function reset working

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let imagem = document.getElementById("principal__container__imagem")
+
 function alterarId (tag, texto1) {
 	let campo = document.getElementById(tag);
 	campo.innerHTML = texto1;
@@ -65,35 +67,59 @@ function descriptografarTexto(texto) {
 function validacao(textoTemp) {
 	let valido = true;
 	for (let i=0; i < textoTemp.value.length; i++ ) {
-		if (textoTemp.value[i] < "a" || textoTemp.value[i] > "z") {
+		if (textoTemp.value[i] != " " && (textoTemp.value[i] < "a" || textoTemp.value[i] > "z")) {
 			valido = false;
+			return valido;
 		}
 	}
 	return valido;
 }
 
 
+function resetar() {
+	alterarId("principal__container__saida", "Seu texto criptografado aparecerá aqui quando você fornecer dados de entrada!");
+	imagem.style.visibility = "visible";
+}
+
 function criptografar() {
 	let texto = document.getElementById("principal__container__entrada");
-
-	if (validacao(texto)){
-		let textoCriptografado = criptografarTexto(texto);
-		alterarId("principal__container__saida", textoCriptografado);
+	if (texto.value.length > 0) {
+		if (validacao(texto)){
+			let textoCriptografado = criptografarTexto(texto);
+			alterarId("principal__container__saida", textoCriptografado);
+			// let imagem = document.getElementById("principal__container__imagem")
+			imagem.style.visibility = "hidden";
+		}
+		else {
+			alterarId("principal__container__saida", "Somente letras minúsculas e espaços serão aceitos, sem caracteres especiais ou acentuação gráfica!");
+			// let imagem = document.getElementById("principal__container__imagem")
+			imagem.style.visibility = "visible";
+		}
 	}
 	else {
-		alterarId("principal__container__saida", "Somente letras minúsculas e espaços serão aceitos, sem caracteres especiais ou acentuação gráfica!");
+		resetar();
 	}
+
 
 }
 
 function descriptografar() {
 	let texto = document.getElementById("principal__container__entrada");
-	if (validacao(texto)){
-		let textoDescriptografado = descriptografarTexto(texto);
-		alterarId("principal__container__saida", textoDescriptografado);
+	if (texto.value.length > 0) {
+		if (validacao(texto)) {
+			let textoDescriptografado = descriptografarTexto(texto);
+			alterarId("principal__container__saida", textoDescriptografado);
+			// let imagem = document.getElementById("principal__container__imagem")
+			imagem.style.visibility = "hidden";
+		}
+		else {
+			alterarId("principal__container__saida", "Somente letras minúsculas e espaços serão aceitos, sem caracteres especiais ou acentuação gráfica!");
+			// let imagem = document.getElementById("principal__container__imagem")
+			imagem.style.visibility = "visible";
+		}
 	}
 	else {
-		alterarId("principal__container__saida", "Somente letras minúsculas e espaços serão aceitos, sem caracteres especiais ou acentuação gráfica!");
+		resetar();
 	}
 
 }
